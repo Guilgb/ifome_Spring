af89dbee7783de68dbc35cb92379e8e869c21461
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-04-28 12:57:44 -0300

    images

--- javaPoo/teste.java
+++ javaPoo/teste.java
@@ -3,13 +3,14 @@ package javaPoo;
 public class teste {
     public static void main(String[] args) {
         Filme avengers = new Filme();
-        avengers.setTitulo("Avenges");
+        avengers.setTitulo("Avengers");
         avengers.setCategoria("Ação");
         avengers.setDuracao(200);
         avengers.setPreco(20);
 
         CadastroFilmes cadFilme = new CadastroFilmes();
         cadFilme.addFilme(avengers);
+        cadFilme.printFilme();
 
     }
 }
